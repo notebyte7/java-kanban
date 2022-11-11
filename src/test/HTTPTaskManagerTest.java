0f178c75bf640f2136d60ff24256e8c24eb2bd8d
notebyte7 <notebyte@yandex.ru>
2022-11-11 23:54:11 +0300

    feat: спринт 8. тесты3

---
+++ src/test/HTTPTaskManagerTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import manager.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import server.HttpTaskServer;
+import server.KVServer;
+
+import java.io.IOException;
+
+class HTTPTaskManagerTest extends TaskManagerTest<TaskManager> {
+    private HttpTaskServer taskServer;
+    private KVServer kvServer = new KVServer();
+
+    public HTTPTaskManagerTest() throws IOException, CrossingTaskException {
+        super(Managers.getDefault());
+    }
+
+    @BeforeEach
+    void setUp() throws IOException, CrossingTaskException {
+        kvServer.start();
+        taskServer = new HttpTaskServer(taskManager);
+        taskManager.load();
+        taskServer.start();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        taskServer.stop();
+        kvServer.stop();
+    }
+
+    @Test
+    void test() throws CrossingTaskException {
+    }
+}
